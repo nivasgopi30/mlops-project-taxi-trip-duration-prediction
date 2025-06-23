d1f14e8aeb59b54346ea3825f79311f1cf608c32
nivasgopi30 <nivasgopi30@gmail.com>
2025-06-23 12:02:59 +0000

    Multiple Files Added.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Sample
+### Prediction of taxi trip duration based on tabular data
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from taxi-trip-duration-prediction!")
+
+
+if __name__ == "__main__":
+    main()
